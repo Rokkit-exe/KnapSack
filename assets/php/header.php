fb9a0b970ebac07f011e6d69abf18be716c79036
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-03-23 16:29:47 -0400

    update structure

---
+++ assets/php/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Knapsak</title>
+</head>
+<body>
+    <header class="container">
+        <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+            <a href="/" class="navbar-brand">Knapsak</a>  
+            <div class="collpase navbar-collapse justify-content-end">
+                <ul class="navbar-nav mr-auto">
+                    <li class="navbar-item">
+                        <a href="index.html" class="nav-link active">About</a>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="index.html" class="nav-link active">Magasin</a>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="index.html" class="nav-link active">Panier</a>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="index.html" class="nav-link active">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    
