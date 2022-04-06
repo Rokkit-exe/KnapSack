ad926693764a30c367a1642686b53bdfbfe48509
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-04-06 11:11:10 -0400

    FDF update affichage panier

--- assets/php/header.php
+++ assets/php/header.php
@@ -1,6 +1,7 @@
 <?php 
     include('function.php');
-    GetPdo();?>
+    GetPdo();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- page/panier.php
+++ page/panier.php
@@ -19,15 +19,12 @@ if(!isset($_SESSION['idJoueur'])){
 <!-- body -->
 
 <div class="container ">
-   
-
-
     <div class='card transaction border border-2 border-success'>
         <div class='card-body text-center'>
             <h2 class='card-title mb-3'>Compléter votre Achat</h2>
                 <div class='mt-3'>
-                    <h4 class='mt-2'>Poids Total: <?=$_SESSION['PoidsTotalePanier']?></h4>
-                    <h4 class='mt-2'>Sous-Total: <?=$_SESSION['PrixTotalePanier']?></h4>
+                    <h4 class='mt-2'>Poids Total: <?=$_SESSION['PoidsPanierTotale']?> lbs</h4>
+                    <h4 class='mt-2'>Sous-Total: <?=$_SESSION['PrixPanierTotale']?>$</h4>
                     <h4 class='mt-2'>Total: 1150$</h4>
                 </div>
         </div>
