81960d7b4688727d7cfe5bed6aa6aa2afa1225e0
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-04-07 21:42:51 -0400

    Modif CSS

--- page/about.php
+++ page/about.php
@@ -3,20 +3,24 @@
 <!-- header -->
 
 <!-- body -->
-<h2>À propos de Knapsack</h2>
-<p style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
-L'application Knapsack est développer dans un univers du jeu <strong>Fallout</strong>.
-Elle vous permet de faire des achats en ligne d'objets, de gérer votre inventaire
-et vos items et de pouvoir évaluer ceux-ci.
-</p>
-<br/>
-<h3>Vos développeurs:</h3>
-<div style="background-color: grey; z-index: 1; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background-image: url(https://wallpapercave.com/wp/wp4799622.jpg)">
-<p>Francis Di-Folco</p>
-<p>Alexandre Carle</p>   
-<p>Kesava Canape Lussier</p>
-<p>Louis-philippe Rousseau</p>
-</div>
+<body style="background-image: url(https://wallpapercave.com/wp/wp4799622.jpg)">
+    <h2 class="espacementTexte">À propos de Knapsack</h2>
+    <div style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
+      <p style="font-size: 16px; margin: 5px">L'application Knapsack est développer dans un univers du jeu <strong style="font-style: italic;">Fallout</strong>.</p>
+      <p style="font-size: 16px; margin: 5px">
+        Elle vous permet de faire des achats en ligne d'objets, de gérer votre inventaire
+        et vos items et de pouvoir évaluer ceux-ci.
+      </p>
+    </div>
+    <br/>
+    <h3 class="espacementTexte">Vos développeurs:</h3>
+    <div class="developpersContainer">
+      <p>- Francis Di-Folco</p>
+      <p>- Alexandre Carle</p>   
+      <p>- Kesava Canape Lussier</p>
+      <p>- Louis-philippe Rousseau</p>
+    </div>
+</body>
 <!-- body -->
 
 <!-- footer -->
