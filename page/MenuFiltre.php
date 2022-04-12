af94935a24687f04d0fdd083ed533c6bc44fd3e5
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-04-12 15:16:42 -0400

    FDF last commit Sprint 1

--- page/MenuFiltre.php
+++ page/MenuFiltre.php
@@ -1,6 +1,6 @@
 <div class="text-center text-light">
         <form class="d-inline p-2 border border-dark rounded mb-3 p-3" method="GET" action="" style="background-color: rgba(33,37,41,0.7)">
-            <select class="me-3 rounded primary" name='type' id='type'>
+            <select class="me-3 rounded" name='type' id='type'>
                 <option value="''" selected>Tout les types d'objets</option>
                 <option value="'Arme'">Armes</option>
                 <option value="'Armure'">Armures</option>
@@ -9,9 +9,9 @@
                 <option value="'Médicament'">Médicaments</option>
             </select>
             <label for="prixMax">Prix Maximum (caps): </label>
-            <input class="me-3 rounded primary" type="number" name="prixMax" id="prixMax" min=<?= PRIX_MIN ?> max=<?= PRIX_MAX ?>>
+            <input class="me-3 rounded" type="number" name="prixMax" id="prixMax" min=<?= PRIX_MIN ?> max=<?= PRIX_MAX ?>>
             <label for="poidsMax">Poids Maximum (lbs): </label>
-            <input class="me-3 rounded primary" type="number" name="poidsMax" id="poidsMax" min=<?= POIDS_MIN ?> max=<?= POIDS_MAX ?>>
+            <input class="me-3 rounded" type="number" name="poidsMax" id="poidsMax" min=<?= POIDS_MIN ?> max=<?= POIDS_MAX ?>>
             <select class="me-3" name='tri' id='tri'>
                 <option value="0" selected>Trier</option>
                 <option value="1">Prix ascendant</option>
