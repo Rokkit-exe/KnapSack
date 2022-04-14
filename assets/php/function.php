f139cfb28d221745bc249ed9be6b93baeb93d923
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-04-14 12:39:26 -0400

    FDF

--- assets/php/function.php
+++ assets/php/function.php
@@ -148,18 +148,6 @@ function getObjet(){
         if($poidsMax == ""){
             $poidsMax = "100";
         }
-/*         if($ordre == "1"){
-            $ordre = 'Objet.Prix ASC';
-        }
-        else if($ordre == "2"){
-            $ordre = 'Objet.Poids ASC';
-        }
-        else if($ordre == "3"){
-            $ordre = 'Objet.Prix DESC';
-        }
-        else if($ordre == "4"){
-            $ordre = 'Objet.Poids DESC';
-        } */
         $sqlProcedure = "CALL AfficherAvecCritère($type , $prixMax , $poidsMax , $ordre)";
     }
     $stmt = $pdo->query($sqlProcedure);
@@ -246,8 +234,6 @@ function getIDJoueur($email){
 
 // modifier la procedure stocker pour augmenter la quantité et non une nouvelle ligne
 function AjouterPanier($idJoueur, $idObjet, $quantité){
-    
-
     $pdo = GetPdo();
     $sql = "CALL AjouterPanier(?,?,?)";
     
