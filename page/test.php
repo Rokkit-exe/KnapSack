d7db69c2314dad2089f5357445afbb6bdbc3ee7a
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-04-22 14:53:28 -0400

    a

--- page/test.php
+++ page/test.php
@@ -4,43 +4,12 @@
 
 <div class="container" >
     <div class="card w-25 text-light mb-2 p-3" style="background-color: rgba(33,37,41,0.7)">
-        <div class="mb-3">
-            <div class="rating mb-2">
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-half"></i>
-                <i class="bi bi-star"></i>
-                4.7 sur 5
-            </div>
-            <div>5 Évaluations Total</div>
-        </div>
-        <div>
-            <div>
-                5 étoiles
-                <progress class="rounded" value="84" max="100"></progress>
-                84%
-            </div>
-            <div>
-                4 étoiles
-                <progress class="rounded" value="29" max="100"></progress>
-                    84%
-            </div>
-            <div>
-                3 étoiles
-                <progress class="rounded" value="18" max="100"></progress>
-                    84%
-            </div>
-            <div>
-                2 étoiles
-                <progress class="rounded" value="35" max="100"></progress>
-                    84%
-            </div>
-            <div>
-                1 étoiles
-                <progress class="rounded" value="2" max="100"></progress>
-                84%
-            </div>
+        <div class="rating mb-2">
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
         </div>
     </div>
 </div>
