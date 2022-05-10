ccf3db6ceab7fc873d5ea211e4150c33672024f1
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-10 15:34:13 -0400

    FDF update enigma

--- page/question.php
+++ page/question.php
@@ -2,25 +2,28 @@
 <?php include('../assets/php/header.php')?>
 <!-- header -->
 
-<div class="container mt-5" style='background-color: rgba(33,37,41,0.7);'>
-    <div class="card text-light">
+<div class="container mt-5 rounded p-5">
+    <div class="text-light" style='background-color: rgba(33,37,41,0.7);'>
         <form action="" method="post">
             <input type="hidden" name="idEnigme" value="<?php echo "idEnigme"; ?>">
-            <h3 class="card-title">Question: <?php echo "question" ;?></h3>
-            <div class="card-body d-block">
-                <label for="reponse"><?php echo "reponse 1" ;?></label>
+            <h3 class="">Question: <?php echo "question" ;?></h3>
+            <div class="">
+                <label for="reponse"><?php echo "this is a reponse to the question reponse 1" ;?></label>
                 <input type="radio" name="reponse" value="<?php echo "idreponse1" ;?>">
-
-                <label for="reponse"><?php echo "reponse 2" ;?></label>
+            </div>
+            <div>
+                <label for="reponse"><?php echo "this is a reponse to the question reponse 2" ;?></label>
                 <input type="radio" name="reponse" value="<?php echo "idreponse2" ;?>">
-
-                <label for="reponse"><?php echo "reponse 3" ;?></label>
+            </div>
+            <div>
+                <label for="reponse"><?php echo "this is a reponse to the question reponse 3" ;?></label>
                 <input type="radio" name="reponse" value="<?php echo "idreponse3" ;?>">
-
-                <label for="reponse"><?php echo "reponse 4" ;?></label>
+            </div>
+            <div>
+                <label for="reponse"><?php echo "this is a reponse to the question reponse 4" ;?></label>
                 <input type="radio" name="reponse" value="<?php echo "idreponse4" ;?>">
             </div>
-            <button>Soumettre</button>
+            <button class="btn btn-primary">Soumettre</button>
         </form>
     </div>
 </div>
