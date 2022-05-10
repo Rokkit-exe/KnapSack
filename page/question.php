58fd33c7ed970d51f24689ed6dd404c5c1ec4339
Rokkit-exe <63754655+Rokkit-exe@users.noreply.github.com>
2022-05-10 15:25:33 -0400

    FDF update enigma

--- page/question.php
+++ page/question.php
@@ -2,17 +2,23 @@
 <?php include('../assets/php/header.php')?>
 <!-- header -->
 
-<div class="container mt-5">
-    <div class="card">
+<div class="container mt-5" style='background-color: rgba(33,37,41,0.7);'>
+    <div class="card text-light">
         <form action="" method="post">
-            <input type="hidden" name="idEnigme" value="">
-            <input type="hidden" name="idReponse" value="">
+            <input type="hidden" name="idEnigme" value="<?php echo "idEnigme"; ?>">
             <h3 class="card-title">Question: <?php echo "question" ;?></h3>
-            <div class="card-body">
-                <label for="idReponse1"><?php echo "reponse 1" ;?></label><input type="radio" name="idReponse1" id="">
-                <label for="idReponse2"><?php echo "reponse 2" ;?></label><input type="radio" name="idReponse2" id="">
-                <label for="idReponse3"><?php echo "reponse 3" ;?></label><input type="radio" name="idReponse3" id="">
-                <label for="idReponse4"><?php echo "reponse 4" ;?></label><input type="radio" name="idReponse4" id="">
+            <div class="card-body d-block">
+                <label for="reponse"><?php echo "reponse 1" ;?></label>
+                <input type="radio" name="reponse" value="<?php echo "idreponse1" ;?>">
+
+                <label for="reponse"><?php echo "reponse 2" ;?></label>
+                <input type="radio" name="reponse" value="<?php echo "idreponse2" ;?>">
+
+                <label for="reponse"><?php echo "reponse 3" ;?></label>
+                <input type="radio" name="reponse" value="<?php echo "idreponse3" ;?>">
+
+                <label for="reponse"><?php echo "reponse 4" ;?></label>
+                <input type="radio" name="reponse" value="<?php echo "idreponse4" ;?>">
             </div>
             <button>Soumettre</button>
         </form>
